58ee3d4fc1f91d726d1d41efcab0387c1406b2a8
msenior526 <msenior526@gmail.com>
2021-06-01 15:30:30 -0400

    Create migration for jewelry products

---
+++ backend/db/schema.rb
@@ -0,0 +1,27 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_06_01_192131) do
+
+  create_table "jewelry_products", force: :cascade do |t|
+    t.string "name"
+    t.string "font_family"
+    t.string "size"
+    t.string "metal_type"
+    t.integer "quantity"
+    t.integer "cart_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_jewelry_products_on_cart_id"
+  end
+
+end
