03407518c87e79ed905c8afb51f67d0c6bfaf585
msenior526 <msenior526@gmail.com>
2021-06-01 15:39:45 -0400

    Create validations for jewelry products

--- backend/app/models/jewelry_product.rb
+++ backend/app/models/jewelry_product.rb
@@ -1,2 +1,6 @@
 class JewelryProduct < ApplicationRecord
+    validates :font_family, :size, presence: true
+    validates :name, presence: true, length: {maximum: 8}
+    validates :quantity, numericality: true
+    validates :metal_type, inclusion: {in: ["gold", "silver", "rose gold", "white gold"]}
 end
