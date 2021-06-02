466f9b83986f73474af8236f3c3a263e52dad5db
msenior526 <msenior526@gmail.com>
2021-06-02 18:12:44 -0400

    Create JewelryProduct class, and form for products

--- frontend/global.js
+++ frontend/global.js
@@ -1,21 +1,31 @@
-let jewelryForm = `
-<h4>Form for Custom Jewelery</h4>
-<img src='images/ring-size-chart.jpeg' alt="Ring Size Chart" id="ring-size-chart">
-<form>
-    <label for='name'>Name:</label>
-    <input type='text' id="custom-name" placeholder="Custom Name"><br>
-    
-    <label for='font-family'>Font Family:</label>
-    <input type='text' id='custom-font-family'><br>
-    
-    <label for='metal-type'>Metal-type:</label>
-    <input type='text' id='custom-metal-type'><br>
-    
-    <label for='size'>Size:</label>
-    <input type='text' id='custom-size'><br>
-    
-    <label for='quantity'>Quantity:</label>
-    <input type='number' id='custom-quantity'><br>
+class JeweleryProduct {
+    constructor(name, metal_type, jewelry_type) {
+        this.name = name,
+        this.metal_type = metal_type,
+        this.jewelry_type = jewelry_type
+    }
 
-    <input type='submit' value="Add To Cart">
-</form>`;
+    static form() {
+        return `
+            <h4>Form for Custom Jewelery</h4>
+            <img src='images/ring-size-chart.jpeg' alt="Ring Size Chart" id="ring-size-chart">
+            <form>
+                <label for='name'>Name:</label>
+                <input type='text' id="custom-name" placeholder="Custom Name"><br>
+        
+                <label for='font-family'>Font Family:</label>
+                <input type='text' id='custom-font-family'><br>
+        
+                <label for='metal-type'>Metal-type:</label>
+                <input type='text' id='custom-metal-type'><br>
+        
+                <label for='size'>Size:</label>
+                <input type='text' id='custom-size'><br>
+        
+                <label for='quantity'>Quantity:</label>
+                <input type='number' id='custom-quantity'><br>
+
+                <input type='submit' value="Add To Cart">
+            </form>`;
+    }
+}
