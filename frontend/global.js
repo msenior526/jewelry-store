b558b365a6ae5c29b84f567733416589498faefa
msenior526 <msenior526@gmail.com>
2021-06-01 20:02:53 -0400

    Create jewelry form

---
+++ frontend/global.js
@@ -0,0 +1,21 @@
+let jewelryForm = `
+<h4>Form for Custom Jewelery</h4>
+<img src='images/ring-size-chart.jpeg' alt="Ring Size Chart" id="ring-size-chart">
+<form>
+    <label for='name'>Name:</label>
+    <input type='text' id="custom-name" placeholder="Custom Name"><br>
+    
+    <label for='font-family'>Font Family:</label>
+    <input type='text' id='custom-font-family'><br>
+    
+    <label for='metal-type'>Metal-type:</label>
+    <input type='text' id='custom-metal-type'><br>
+    
+    <label for='size'>Size:</label>
+    <input type='text' id='custom-size'><br>
+    
+    <label for='quantity'>Quantity:</label>
+    <input type='number' id='custom-quantity'><br>
+
+    <input type='submit' value="Add To Cart">
+</form>`;
