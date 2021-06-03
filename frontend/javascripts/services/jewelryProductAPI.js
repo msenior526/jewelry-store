9206f55ca662f6c5dc722bca81c974c041553588
msenior526 <msenior526@gmail.com>
2021-06-03 13:22:06 -0400

    Edit fetch call

--- frontend/javascripts/services/jewelryProductAPI.js
+++ frontend/javascripts/services/jewelryProductAPI.js
@@ -1,12 +1,9 @@
 class JewelryProductApi {
     static fetchProducts() {
-        let jewelryList = document.getElementById('jewelry-list')
         return fetch("http://localhost:3000/jewelry_products")
         .then(resp => resp.json())
         .then(json => {
-            return json.data.forEach((product) => {
-                console.log(product);
-            })
+            return json.data.forEach((product) => product);
         })
     }
 }
