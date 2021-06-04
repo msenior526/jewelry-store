0d84a03d49308428d25f27e384ca9830119eab73
msenior526 <msenior526@gmail.com>
2021-06-04 15:02:23 -0400

    Create method for all

--- frontend/javascripts/models/jewelryProduct.js
+++ frontend/javascripts/models/jewelryProduct.js
@@ -1,9 +1,14 @@
 class JewelryProduct {
+    static all = []
     constructor(name, price, jewelry_type, metal_type) {
         this.name = name,
         this.price = price,
         this.metal_type = metal_type,
-        this.jewelry_type = jewelry_type
+        this.jewelry_type = jewelry_type,
+        JewelryProduct.all.push(this)
 
     }
+
+   static display() {
+   }
 }
--- frontend/javascripts/services/jewelryProductAPI.js
+++ frontend/javascripts/services/jewelryProductAPI.js
@@ -3,9 +3,10 @@ class JewelryProductApi {
         return fetch("http://localhost:3000/jewelry_products")
         .then(resp => resp.json())
         .then(json => {
-            return json.data.forEach((product) => {
+                return json.data.forEach((product) => {
                 const {name, size, jewelry_type, metal_type} =  product.attributes
                 let jewelry = new JewelryProduct(name, size, jewelry_type, metal_type);
+                return jewelry;
             });
         })
     }
