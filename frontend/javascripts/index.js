c3b0c3474a0b53c4c04c1d5c5f76d81954c59b96
msenior526 <msenior526@gmail.com>
2021-06-04 12:05:30 -0400

    Add click events to images

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', event => {
+
     const renderJewelryForm = () => {
         const form = document.getElementById('jewelry-form');
         form.hidden = false;
         return form;
     }
+
     let personalizeLink = document.getElementById('personalize-link');
-    personalizeLink.addEventListener('click', e => {
-        renderJewelryForm(jewelryForm())
-    })
+
+    
+    const images = document.getElementsByClassName('jewelry-img')
+        for (i = 0; i < images.length; i++) {
+        images[i].addEventListener('click', e => { 
+            return renderJewelryForm(jewelryForm())
+        })
+        }
 })
+        
