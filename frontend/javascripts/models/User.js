df23802cb57bc3e7048d95bdf3712d795bcbc294
msenior526 <msenior526@gmail.com>
2021-06-08 20:30:01 -0400

    Create display function

--- frontend/javascripts/models/User.js
+++ frontend/javascripts/models/User.js
@@ -1,6 +1,15 @@
 class User {
+    static all = [];
     constructor(id, username) {
         this.id = id, 
-        this.username
+        this.username = username
+    }
+
+    display() {
+        let listItem = document.createElement('li')
+        listItem.innerHTML = `
+        <h3>${this.username}</h3>
+        `
+        return jewelryList.appendChild(listItem);
     }
 }
