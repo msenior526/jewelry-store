4b20693b9e8cb6d97a23803432e893d6ff62c0de
msenior526 <msenior526@gmail.com>
2021-06-08 20:59:50 -0400

    Create variables for user submit and list

--- frontend/javascripts/global.js
+++ frontend/javascripts/global.js
@@ -1,4 +1,5 @@
 const submitButton = document.querySelector("input[value='Add To Cart']");
+const submitUserBtn = document.querySelector("input[value='Submit']");
 const nameInput = document.querySelector('input#jewelry-product-name');
 const fontInput = document.querySelector('input#jewelry-product-font');
 const metalInput = document.querySelector('input#jewelry-product-metal');
@@ -8,3 +9,4 @@ const typeInput = document.getElementById('jewelry-product-type')
 const usernameInput = document.getElementById('username')
 const jewelryForm = () => document.querySelector('div#jewelry-form');
 const jewelryList = document.getElementById('jewelry-list');
+const userList = document.getElementById('user-list');
