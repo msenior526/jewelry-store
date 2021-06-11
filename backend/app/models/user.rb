23179a6f53d536ab59f67a07e1739c4600cb839d
msenior526 <msenior526@gmail.com>
2021-06-11 17:05:43 -0400

    Add has_secure_password to model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :jewelry_products
     has_many :shopping_carts
+    has_secure_password
 
     validates :username, uniqueness: true, presence: true, length: {in: 4..12}
 end
