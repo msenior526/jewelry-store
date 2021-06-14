5d1140b3fbe734e2515c4fab3efdcf4ecb6f960c
msenior526 <msenior526@gmail.com>
2021-06-14 14:10:03 -0400

    Move function from index.js to jewelryproduct.js

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -16,23 +16,8 @@ document.addEventListener('DOMContentLoaded', event => {
         jewelryForm().style.display = 'none';
     })
     
-    const renderJewelryForm = (e) => {
-        jewelryForm().style.display = 'block';
-        switch (e.target.id) {
-            case "ring-img":
-                typeInput.value = "ring";
-                break;
-            case "necklace-img":
-                typeInput.value = "necklace";
-                break;
-            case "bracelet-img":
-                typeInput.value = "bracelet";
-                break;
-        }
-    }
-    
     for (let i = 0; i < images.length; i++) {
-        images[i].addEventListener('click', renderJewelryForm)
+        images[i].addEventListener('click', JewelryProduct.renderJewelryForm)
     }
 })
         
--- frontend/javascripts/models/jewelryProduct.js
+++ frontend/javascripts/models/jewelryProduct.js
@@ -32,6 +32,21 @@ class JewelryProduct {
             e.target.reset();
     }
 
+    static renderJewelryForm = (e) => {
+        jewelryForm().style.display = 'block';
+        switch (e.target.id) {
+            case "ring-img":
+                typeInput.value = "ring";
+                break;
+            case "necklace-img":
+                typeInput.value = "necklace";
+                break;
+            case "bracelet-img":
+                typeInput.value = "bracelet";
+                break;
+        }
+    }
+
     static findById(id) {
         return this.all.filter(prod => prod.id === id)
     }
