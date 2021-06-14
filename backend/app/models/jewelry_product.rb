f26bdbe85f1e81b8d7fcdc35b9e0cea4294e1814
msenior526 <msenior526@gmail.com>
2021-06-14 12:08:17 -0400

    Remove quantity from validations and calculate price

--- backend/app/models/jewelry_product.rb
+++ backend/app/models/jewelry_product.rb
@@ -5,32 +5,31 @@ class JewelryProduct < ApplicationRecord
     
     validates :font_family, :size, presence: true
     validates :name, presence: true, length: {maximum: 8}
-    validates :quantity, numericality: true
     validates :metal_type, inclusion: {in: ["yellow gold", "silver", "rose gold", "white gold"]}
     validates :jewelry_type, inclusion: {in: ["necklace", "ring", "bracelet"]}
 
     def calculate_price
         new_price = 0
         if ((self.metal_type == 'yellow gold' || self.metal_type == 'white gold') && self.jewelry_type == 'necklace')
-                new_price = 150.00 * self.quantity
+                new_price = 150.00
         elsif (self.metal_type == 'rose gold' && self.jewelry_type == 'necklace')
-                new_price = 140.00 * self.quantity 
+                new_price = 140.00 
         elsif (self.metal_type == 'silver' && self.jewelry_type == 'necklace')
-                new_price = 125.00 * self.quantity 
+                new_price = 125.00 
         
         elsif ((self.metal_type == 'yellow gold' || self.metal_type == 'white gold') && self.jewelry_type == 'bracelet')
-                new_price = 100.00 * self.quantity 
+                new_price = 100.00 
         elsif (self.metal_type == 'rose gold' && self.jewelry_type == 'bracelet')
-                new_price = 90.00 * self.quantity 
+                new_price = 90.00 
         elsif (self.metal_type == 'silver' && self.jewelry_type == 'bracelet')
-                new_price = 75.00 * self.quantity 
+                new_price = 75.00 
  
         elsif ((self.metal_type == 'yellow gold' || self.metal_type == 'white gold') && self.jewelry_type == 'ring')
-                new_price = 100.00 * self.quantity 
+                new_price = 100.00 
         elsif (self.metal_type == 'rose gold' && self.jewelry_type == 'ring')
-                new_price = 90.00 * self.quantity 
+                new_price = 90.00 
         elsif (self.metal_type == 'silver' && self.jewelry_type == 'ring')
-                new_price = 75.00 * self.quantity 
+                new_price = 75.00 
         end 
         return new_price
     end
